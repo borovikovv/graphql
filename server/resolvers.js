3864312f3a68a81b2e61775d652ade3813306e82
Oleksii Borovykov <borovikovvaleksey@gmail.com>
2023-06-28 13:54:12 +0300

    add company query

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,16 @@
 import {getJobs} from "./db/jobs.js";
+import {getCompany} from "./db/companies.js";
 
 export const resolvers = {
   Query: {
-    jobs: async () => await getJobs()
+    jobs: async () => await getJobs(),
+  },
+  Job: {
+    company: async (job) => await getCompany(job.companyId),
+    date: (job) => toIsoDate(job.createdAt),
   }
- }
+}
+
+function toIsoDate(date) {
+  return date.slice(0, 'yyyy-mm-dd'.length)
+}
